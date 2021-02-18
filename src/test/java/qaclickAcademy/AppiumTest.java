8eae67aca427aac834aa800f74e1c30832fd39c4
netravati <netra.alagundi@gmail.com>
2021-02-18 13:38:38 +0530

    first commit

---
+++ src/test/java/qaclickAcademy/AppiumTest.java
@@ -0,0 +1,17 @@
+package qaclickAcademy;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+	
+	@Test
+	public void NativeAppAndroid() {
+		System.out.println("NativeAppAndroid");
+	}
+	
+	@Test
+	public void iOSApp() {
+		System.out.println("iOSApp");
+	}
+
+}
